b8064b467c18f6bce20fae96b47e02eec0309890
Russell Holbrook <russell.holbrook@me.com>
2022-12-04 10:39:29 -0500

    Day 4 part 2 also before brunch!

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%86/yellow)](src/day04)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -90,9 +90,9 @@ Both parts: 1.459041ms
 
 ```
 Day 04
-Time part 1: 12.21ms
-Time part 2: -
-Both parts: 12.209791ms
+Time part 1: 11.156ms
+Time part 2: 8.698ms
+Both parts: 19.853834ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 7/50
-Total time: 15.292ms
+Total stars: 8/50
+Total time: 22.936ms
 ```
 
 <!--/RESULTS-->
--- src/day04/index.ts
+++ src/day04/index.ts
@@ -3,14 +3,18 @@ import { sum, eqSet, intersect, range } from "../utils/index.js";
 
 const parseInput = (rawInput: string) => rawInput;
 
+const pairInput = (input: string) => {
+  return input.split('\n').map(l => l.split(',', 2).map(e => {
+    const rangePair = e.split('-', 2).map(n => parseInt(n, 10)) as [number, number];
+    // if(rangePair.length > 2) throw Error("Invalid Range: " + rangePair.toString());
+    return new Set(range(rangePair[0], rangePair[1]));
+  }));
+}
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  const pairedInput = input.split('\n').map(l => l.split(',', 2).map(e => {
-      const rangePair = e.split('-', 2).map(n => parseInt(n, 10)) as [number, number];
-      // if(rangePair.length > 2) throw Error("Invalid Range: " + rangePair.toString());
-      return new Set(range(rangePair[0], rangePair[1]));
-    }));
+  const pairedInput = pairInput(input);
 
   return pairedInput.map(l => {
     const inter = intersect(l[0], l[1]);
@@ -21,7 +25,9 @@ const part1 = (rawInput: string) => {
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const pairedInput = pairInput(input);
+
+  return pairedInput.map(l => +(intersect(l[0], l[1]).size > 0)).reduce(sum, 0);
 };
 
 run({
@@ -41,10 +47,15 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8`,
+        expected: 4,
+      },
     ],
     solution: part2,
   },
