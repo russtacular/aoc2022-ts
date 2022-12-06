0b06c76fb85a78c0995f280842999c0d4b88e7b7
Russell Holbrook <russell.holbrook@me.com>
2022-12-06 10:29:29 -0500

    Shim interface for Array.prototype.findLastIndex
    Typescript doesn't yet support it (but should be in the next release)

---
+++ src/utils/global.d.ts
@@ -0,0 +1,12 @@
+// Support for Array.findLastIndex hasn't been added to TS yet
+
+export {}
+
+declare global {
+  interface Array<T> {
+    findLastIndex(
+      predicate: (value: T, index: number, obj: T[]) => unknown,
+      thisArg?: any
+    ): number
+  }
+}
